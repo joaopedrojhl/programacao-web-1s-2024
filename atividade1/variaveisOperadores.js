9f13ae22d44a4817cded9e18075e600e6345f797
JOÃO PEDRO GUIMARÃES BONFIM <joao.bonfim@a.ucb.br>
2024-03-13 10:04:00 -0300

    add atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,9 @@
+const a = 2;
+const b = 3;
+
+let c = a + b;
+
+console.log('O resultado da soma entre ' + a + 'e' + b + 'é' + c);
+
+c = a - b;
+console.log(`O resultado da subtração entre $(a) e $(b) é $(c));
