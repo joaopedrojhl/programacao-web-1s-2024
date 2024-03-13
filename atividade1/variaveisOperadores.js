9b0d8eab1c9db72804e363cef41f6353c9874f23
JOÃO PEDRO GUIMARÃES BONFIM <joao.bonfim@a.ucb.br>
2024-03-13 10:10:55 -0300

    VariaveisOperadores

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,9 +1,13 @@
 const a = 2;
 const b = 3;
-
+const d = "2";
+const e = "3";
+ 
 let c = a + b;
 
-console.log('O resultado da soma entre ' + a + 'e' + b + 'é' + c);
+console.log('O resultado da soma entre ' + a + ' e ' + b + ' é ' + c);
 
 c = a - b;
-console.log(`O resultado da subtração entre $(a) e $(b) é $(c));
+console.log(`O resultado da subtração entre ${a} e ${b} é ${c}`);
+
+console.log(`O resultado da subtração entre ${d} e ${e} é ${d - e}`);
