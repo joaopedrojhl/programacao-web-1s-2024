87626aa7e483e48c175cc329c4247cebd04e6f55
JOÃO PEDRO GUIMARÃES BONFIM <joao.bonfim@a.ucb.br>
2024-03-20 10:58:47 -0300

    .

--- objeto/objeto.js
+++ objeto/objeto.js
@@ -10,3 +10,22 @@ calculadora.numneroA = 2;
 calculadora.numeroB = 3
 
 console.log(calculadora.somar());
+
+
+const pessoa = {
+    nome: "Nome da pessoa",
+    cpf: 123456789,
+    telefone: 99999999
+}
+
+for(const key in pessoa){
+    console.log(key + ' - ' + pessoa[key]);
+}
+
+let arr = [false, 1, "DOIS", 11, 56, 60];
+
+for(let i = 0; i< arr.length; i++){
+    console.log(i + ' - ' + arr[i]);
+}
+
+let matriz = [[1, 2], [3, 4]];
