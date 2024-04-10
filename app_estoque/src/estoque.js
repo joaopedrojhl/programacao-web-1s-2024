1e3cf6c2791efe9da0471d8250e6934f74f67e60
JOÃO PEDRO GUIMARÃES BONFIM <joao.bonfim@a.ucb.br>
2024-04-10 11:09:53 -0300

    estoque

---
+++ app_estoque/src/estoque.js
@@ -0,0 +1,31 @@
+let produtos = [];
+
+function criarProduto(id, nome, qtd) {
+    let p ={
+        id: id,
+        nome: nome,
+        qtd: qtd
+    }    
+    return p;
+}
+
+function adicionarProduto(p){
+    produtos.push(p);
+}
+
+function listaProdutos() {
+    return produtos;
+}
+
+function removerProduto(id) {
+    produtos = produtos.filter((p) => {
+        return p.id != id
+    }); 
+}
+
+module.exports = {
+    criarProduto,
+    adicionarProduto,
+    listaProdutos,
+    removerProduto
+};
